7ca236ef8f217597e8f3214e2481b0eafa797663
Natakorn Phukvararat <005808@cdgs.co.th>
2021-10-01 14:58:20 +0700

    add test parameter

--- src/test/java/com/example/day02/employee/EmployeeControllerTest.java
+++ src/test/java/com/example/day02/employee/EmployeeControllerTest.java
@@ -1,6 +1,8 @@
 package com.example.day02.employee;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -14,13 +16,22 @@ public class EmployeeControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+
     @Test
     public void getById() {
-        // Act
-        EmployeeResponse result
-                = restTemplate.getForObject("/employee/1", EmployeeResponse.class);
-        // Assert
+        EmployeeResponse result = restTemplate.getForObject("/employee/1", EmployeeResponse.class);
         assertEquals(1, result.getId());
-        assertEquals("Nekko", result.getName());
+        assertEquals("Nekko1", result.getName());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1",
+            "2",
+    })
+    public void getByIdList(int id) {
+        EmployeeResponse result = restTemplate.getForObject("/employee/"+id, EmployeeResponse.class);
+        assertEquals(id, result.getId());
+        assertEquals("Nekko"+id, result.getName());
     }
 }
